623ba7741535858465c59a29ffe63308b4a65e41
mumuwocal <153215796+mumuwocal@users.noreply.github.com>
2023-12-08 01:06:21 +0800

    Create 1.php

---
+++ 1.php
@@ -0,0 +1,14 @@
+<?php
+// 开始 PHP 代码
+echo "<!DOCTYPE html>\n";
+echo "<html>\n";
+echo "<head>\n";
+echo "    <title>欢迎页面</title>\n";
+echo "</head>\n";
+echo "<body>\n";
+echo "    <h1>欢迎访问我的网站！</h1>\n";
+echo "    <p>这是一个纯PHP生成的欢迎页面。</p>\n";
+echo "</body>\n";
+echo "</html>\n";
+// 结束 PHP 代码
+?>
